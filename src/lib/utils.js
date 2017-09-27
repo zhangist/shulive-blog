bd049974969a262476afd1136c8b156866df1a34
zhangyongbin <zhangist@163.com>
2017-09-27 23:00:30 +0800

    readme

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,22 @@
-const dateFormat = (obj, fmt) => {
+/**
+* @param {String} string
+* @param {Number} targetLength
+* @return {String}             Return convert result
+*/
+function addZeros(str, targetLength = 2) {
+  let strRes = str;
+  while (str.length < targetLength) {
+    strRes = `0${strRes}`;
+  }
+  return strRes;
+}
+
+/**
+* @param  {obj} object
+* @param  {String} format
+* @return {String}           Return format result
+*/
+const dateFormat = (obj, fmt = 'YYYY-MM-DD hh:mm:ss') => {
   let newFmt = fmt;
   const o = {
     'M+': obj.getMonth() + 1, // 月份
@@ -21,4 +39,28 @@ const dateFormat = (obj, fmt) => {
   return newFmt;
 };
 
-exports.dateFormat = dateFormat;
+/**
+* @param  {String} dateStr
+* @return {Number}         Return timestamp
+*/
+function parseTimestamp(dateStr) {
+  const rShortMatch = /^\s*(\d{4})-(\d{1,2})-(\d{1,2})\s*$/;
+  const rLongMatch = /^\s*(\d{4})-(\d{1,2})-(\d{1,2})\s+(\d{1,2}):(\d{1,2}):(\d{1,2})\s*$/;
+  let match;
+  let timestamp;
+
+  if (dateStr.match(rShortMatch)) {
+    match = dateStr.match(rShortMatch);
+    timestamp = +new Date(+match[1], +match[2] - 1, +match[3]);
+  } else if (dateStr.match(rLongMatch)) {
+    match = dateStr.match(rLongMatch);
+    timestamp = +new Date(+match[1], +match[2] - 1, +match[3], +match[4], +match[5], +match[6]);
+  }
+  return timestamp;
+}
+
+module.exports = {
+  dateFormat,
+  parseTimestamp,
+  addZeros,
+};
